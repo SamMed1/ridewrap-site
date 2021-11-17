366e9ae5a40667ada428f3c3933bf3bb74feb142
Sam Medhurst <sam@ridewrap.ca>
2021-11-17 23:17:54 +0000

    add code in files types: .php,.js remove code in files types: .php,.js

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 console.log("rwcxh")
 console.log("tdlak")
-console.log("dmvah")
+console.log("btrao")
--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <?php
         echo "kqojc";
         echo "rtjah";
-        echo "lwdhs";
+        echo "lnnir";
 ?>
