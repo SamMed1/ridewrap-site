5d59d571293c1344dafc762b971cc88ef5b0a805
Sam Medhurst <sam@ridewrap.ca>
2022-01-24 22:13:27 +0000

    add code in files types: .css,.php,.map,.js,.scss remove code in files types: .css,.php,.map,.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 console.log("hielq")
 console.log("vvogr")
 console.log("ebuvm")
-console.log("uulry")
+console.log("ltmep")
--- content.php
+++ content.php
@@ -11,5 +11,5 @@
         echo "brwey";
         echo "mijfy";
         echo "aychm";
-        echo "nspyu";
+        echo "konar";
 ?>
